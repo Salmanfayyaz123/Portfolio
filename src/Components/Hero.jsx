4efdebf3b56ab63570a5cdb409df310c77b924ee
Salmanfayyaz123 <143013749+Salmanfayyaz123@users.noreply.github.com>
2025-07-15 11:38:51 +0500

    new resume added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           </p>
 
           <p className="text-base sm:text-lg md:text-xl">
-            I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 7 years, I still love it as if it was something new.
+            I'm a full stack developer (React.js & Node.js) with a focus on creating (and occasionally designing) exceptional digital experiences that are fast, accessible, visually appealing, and responsive. Even though I have been creating web applications for over 2 years, I still love it as if it was something new.
           </p>
 
           {/* location and availability */}
@@ -52,7 +52,7 @@ const Hero = () => {
         </div>
 
       </div>
-    </div>
+    </div>  
   );
 };
 
